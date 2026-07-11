6bdb400ae36b0fc1624421820cd3542143e07c67
sakuni rajapaksha <sakunirajapaksha007@gmail.com>
2026-07-11 15:57:58 +0530

    Fix production API URL

--- src/app/lib/socket.js
+++ src/app/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  "http://https://d-entertainment-backend.onrender.com",
+  "https://d-entertainment-backend.onrender.com",
 );
 
 export default socket;
